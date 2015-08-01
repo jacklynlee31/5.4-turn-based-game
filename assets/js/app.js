339f6e9ffc9e56264a4d9a28c943d4a9ff849d6f
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-08-01 10:33:24 -0500

    Still on JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
 $('.start').html(AppTemplates.start(heroes));
 
+$('.start').click(function() {
+  html = ('.battle');
+});
+
 $(game).html(AppTemplates.start());
 $(game).html(AppTemplates.battle());
 $(game).html(AppTemplates.end());
