155ee40ce4e7c51213170356f2f0cea104daef38
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-08-01 12:50:21 -0500

    Another attempt

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,23 @@
-$('.start').html(AppTemplates.start(heroes));
+$('.game').html(AppTemplates.start(heroes));
 
-$('.start').click(function() {
-  html = ('.battle');
+$('.start-name').click(function() {
+  $('.start').html(AppTemplates.battle());
 });
 
-$(game).html(AppTemplates.start());
-$(game).html(AppTemplates.battle());
-$(game).html(AppTemplates.end());
+// $('.start-name').on('click', function() {
+//   $('.start').html(AppTemplates.battle());
+// });
+
+  // .html('div.battle');
+
+  // $('.game').html(div.battle);
+
+// $('.start').html(AppTemplates.start(heroes));
+
+// $('.start').click(function() {
+//   html = ('.battle');
+// });
+
+// $(game).html(AppTemplates.start());
+// $(game).html(AppTemplates.battle());
+// $(game).html(AppTemplates.end());
