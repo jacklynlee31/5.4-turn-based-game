8c658a383964a99dd703643359a97bae7ad95dae
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-08-02 14:11:26 -0500

    Working on index

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,13 @@
 $('.game').html(AppTemplates.start(heroes));
 
 $('.start-name').click(function() {
-  var game = new Game(heroes[0]);
+  var index = $(this).data('index');
+  var game = new Game(heroes[index]);
   $('.game').html(AppTemplates.battle(game));
 });
 
 // A User should be able to choose a character for their game
 
 // A User should see the battle screen when a character is clicked
+
+// var game = new Game(enemies[0]);
